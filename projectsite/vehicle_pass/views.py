fc04354ff8d4ea5b588af143a0e8cc302de79037
Rex Gatchalian <rexg744@gmail.com>
2025-03-18 03:17:32 +0800

    revised models -rex

--- projectsite/vehicle_pass/views.py
+++ projectsite/vehicle_pass/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegistrationForm, UserProfileForm, SecurityProfileForm, CashierProfileForm, AdminProfileForm
+from .forms import VehicleForm, RegistrationForm, RegistrationStatusForm, VehiclePassForm, PaymentTransactionForm
+from .forms import InspectionReportForm, NotificationForm, AnnouncementForm
 from .models import UserProfile, SecurityProfile, CashierProfile, AdminProfile
-from .models import Vehicle, Registration, RegistrationStatus, VehiclePass, PaymentTransaction
+from .models import Vehicle, Registration, VehiclePass, PaymentTransaction
 from .models import InspectionReport, Notification, Announcement
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 def register(request):
     if request.method == 'POST':
@@ -43,37 +47,78 @@ def register(request):
 class UserPageView(ListView):
     model = UserProfile
     context_object_name = "user"
-    template_name = ""
+    template_name = "user.html"
+
+
 
 class SecurityPageView(ListView):
     model = SecurityProfile
     context_object_name = "security"
-    template_name = ""
+    template_name = "security.html"
 
 class CashierPageView(ListView):
     model = CashierProfile
     context_object_name = "cashier"
-    template_name = ""
+    template_name = "cashier.html"
 
 class AdminPageView(ListView):
     model = AdminProfile
     context_object_name = "admin"
-    template_name = ""
+    template_name = "admin.html"
 
+######VEHICLE REGISTRATION VIEW AND CRUD ############
+#####################################################
 class VehiclePageView(ListView):
     model = Vehicle 
     context_object_name = "vehicle"
-    template_name = ""
+    template_name = "vehicle.html"
 
+class VehicleCreateView(CreateView):
+    model = Vehicle
+    form_class = VehicleForm
+    template_name = "vehicle_form.html"
+    success_url = reverse_lazy('vehicle')
+
+    def form_valid(self,form):
+        plate_number = form.instance.plate_Number
+        messages.success(self.request, f'{plate_number} has been successfully updated.')
+
+        return super().form_valid(form)
+
+class VehicleUpdateView(UpdateView):
+    model = Vehicle
+    form_class = VehicleForm
+    template_name = "vehicle_edit.html"
+    success_url = reverse_lazy('vehicle')
+
+    def form_valid(self,form):
+        plate_number = form.instance
+        messages.success(self.request, f'{plate_number} has been successfully updated.')
+
+        return super().form_valid(form)
+    
+class VehicleDeleteView(DeleteView):
+    model = Vehicle
+    template_name = "vehicle_delete.html"
+    success_url = reverse_lazy('vehicle')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Successfully deleted.')
+        return super().form_valid(form)
+
+
+
+######REGISTRATION VIEW AND CRUD
+################################   
 class RegistrationView(ListView):
     model = Registration
     context_object_name = "registration"
     template_name = ""
 
-class RegistrationStatusView(ListView):
-    model = RegistrationStatus
-    context_object_name = "registration_status"
-    template_name = ""
+# class RegistrationStatusView(ListView):
+#     model = RegistrationStatus
+#     context_object_name = "registration_status"
+#     template_name = ""
 
 class VehiclePassView(ListView):
     model = VehiclePass
