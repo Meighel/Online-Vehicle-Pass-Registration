c779e7497c238f6ffdf15471883612cc158a1cdd
Kenjicci <Cincokencstd1@gmail.com>
2025-05-17 15:34:51 +0800

    added forgot password feature

---
+++ projectsite/static/javascript/Create_Acct_Validation.js
@@ -0,0 +1,11 @@
+function validateCorpEmail() {
+    const CorpEmail = document.getElementById("email").value; 
+
+    const CorpEmailErr = document.getElementById("emailErr").value;
+
+    is_valid = true
+
+
+
+
+}
